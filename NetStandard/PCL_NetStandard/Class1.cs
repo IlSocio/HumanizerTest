9604dd88f20efd0d786871083dce14840176e245
Marco Bellino <m.bellino@symbian-toys.com>
2016-07-15 14:27:36 +0200

    Initial commit

---
+++ NetStandard/PCL_NetStandard/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Humanizer;
+
+namespace PCL_NetStandard
+{
+    public class Class1
+    {
+        string test = "".Pluralize();
+    }
+}
